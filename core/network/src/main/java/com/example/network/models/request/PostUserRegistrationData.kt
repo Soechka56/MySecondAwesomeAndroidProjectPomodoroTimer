1a72fb9c65bb1edef22b4c04654b5693dae6533a
soechka1flow <soechka1flow@gmail.com>
2026-04-22 23:41:45 +0300

    very complex task, spent 2 hours..

--- core/network/src/main/java/com/example/network/models/request/PostUserRegistrationData.kt
+++ core/network/src/main/java/com/example/network/models/request/PostUserRegistrationData.kt
@@ -1,8 +1,14 @@
 package com.example.network.models.request
 
+import com.google.gson.annotations.SerializedName
+
 data class PostUserRegistrationData(
+    @SerializedName("email")
     val email: String,
+    @SerializedName("username")
     val username: String,
+    @SerializedName("full_name")
     val fullName: String,
+    @SerializedName("password")
     val password: String,
 )
