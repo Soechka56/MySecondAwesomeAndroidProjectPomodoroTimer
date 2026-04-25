bd2bf534b41ddfb80a0cd19e254dd187da8d3a21
soechka1flow <soechka1flow@gmail.com>
2026-04-25 15:10:30 +0300

    fix gradle config

--- feature/user-profile/impl/build.gradle.kts
+++ feature/user-profile/impl/build.gradle.kts
@@ -1,33 +1,9 @@
 plugins {
-    alias(libs.plugins.android.library)
+    alias(libs.plugins.app.android.library)
 }
 
 android {
-    namespace = "com.example.impl"
-    compileSdk {
-        version = release(36)
-    }
-
-    defaultConfig {
-        minSdk = 26
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
+    namespace = "com.example.user_profile.impl"
 }
 
 dependencies {
