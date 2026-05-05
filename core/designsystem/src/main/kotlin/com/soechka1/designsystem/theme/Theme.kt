f46757a315c4953ef61ea954fc44e10b79a1bd1c
soechka1flow <soechka1flow@gmail.com>
2026-05-05 15:17:45 +0300

    migration to Material 3 Expressive

--- core/designsystem/src/main/kotlin/com/soechka1/designsystem/theme/Theme.kt
+++ core/designsystem/src/main/kotlin/com/soechka1/designsystem/theme/Theme.kt
@@ -3,7 +3,9 @@ package com.soechka1.designsystem.theme
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.ColorScheme
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
+import androidx.compose.material3.MaterialExpressiveTheme
+import androidx.compose.material3.MotionScheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
@@ -74,6 +76,7 @@ object PomodoroTheme {
 }
 
 @Composable
+@OptIn(ExperimentalMaterial3ExpressiveApi::class)
 fun MySecondAwesomeAndroidProjectPomodoroTimerTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     dynamicColor: Boolean = false,
@@ -99,8 +102,9 @@ fun MySecondAwesomeAndroidProjectPomodoroTimerTheme(
         LocalPomodoroShapes provides shapes,
         LocalPomodoroSpacing provides DefaultPomodoroSpacing,
     ) {
-        MaterialTheme(
+        MaterialExpressiveTheme(
             colorScheme = colorScheme,
+            motionScheme = MotionScheme.expressive(),
             typography = materialTypography(pomodoroColors),
             shapes = materialShapes(),
             content = content,
